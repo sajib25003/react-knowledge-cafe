f6aef805e883db7f4293446cef8d6cbca1ee57bf
Md Ashik Hassan Bhuiyan <sajib.25003@Sajibs-MacBook-Air.local>
2024-03-15 01:47:18 +0600

    structure prepared

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Blogs from "./components/Blogs/Blogs";
+import Header from "./components/Header";
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Blogs></Blogs>
+    </>
+  );
+}
+
+export default App;
