94885ca98c802c7b44a2269e91e6520cda0bc8da
Md Ashik Hassan Bhuiyan <sajib.25003@Sajibs-MacBook-Air.local>
2024-03-15 01:56:35 +0600

    useState and data fetching done

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,5 +1,12 @@
-const Blogs = () => {
+import { useEffect, useState } from "react";
 
+const Blogs = () => {
+    const [blogs, setBlogs] = useState([]);
+    useEffect( ()=>{
+        fetch('blogs.json')
+            .then(res => res.json())
+            .then(data => setBlogs(data))
+    },[] )
     return (
         <div>
             
